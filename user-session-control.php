3356d3fb7b9ddbadc10aa824eca2dbe694135769
Frankie Jarrett <frankie@x-team.com>
2015-01-07 16:10:13 -0600

    Localize date format display

--- user-session-control.php
+++ user-session-control.php
@@ -166,6 +166,8 @@ function usc_user_submenu_callback() {
 							'_wpnonce'   => wp_create_nonce( sprintf( 'destroy_session_nonce-%d', $user_id ) ),
 						)
 					);
+					$created    = strtotime( get_date_from_gmt( date( 'Y-m-d H:i:s', $result['created'] ) ) );
+					$expiration = strtotime( get_date_from_gmt( date( 'Y-m-d H:i:s', $result['expiration'] ) ) );
 					?>
 					<tr <?php echo ( 0 !== $i % 2 ) ? 'class="alternate"' : '' ?>>
 						<td class="username column-username">
@@ -191,12 +193,12 @@ function usc_user_submenu_callback() {
 						<td>
 							<strong><?php printf( __( '%s ago' ), human_time_diff( $result['created'] ) ) ?></strong>
 							<br>
-							<small><?php echo esc_html( get_date_from_gmt( date( 'Y-m-d H:i:s', $result['created'] ), $date_format ) ) ?></small>
+							<small><?php echo esc_html( date_i18n( $date_format, $created ) ) ?></small>
 						</td>
 						<td>
-							<strong><?php printf( __( 'in %s' ), human_time_diff( $result['expiration'] ) ) ?></strong>
+							<strong><?php printf( __( 'in %s', 'user-session-control' ), human_time_diff( $result['expiration'] ) ) ?></strong>
 							<br>
-							<small><?php echo esc_html( get_date_from_gmt( date( 'Y-m-d H:i:s', $result['expiration'] ), $date_format ) ) ?></small>
+							<small><?php echo esc_html( date_i18n( $date_format, $expiration ) ) ?></small>
 						</td>
 						<td><?php echo esc_html( $result['ip'] ) ?></td>
 					</tr>
