627f2041e2cc9d53b73d8a93c026ac609dcef3fa
Frankie Jarrett <fjarrett@gmail.com>
2016-03-29 22:17:42 -0500

    Update user-session-control.php

--- user-session-control.php
+++ user-session-control.php
@@ -7,6 +7,10 @@
  * Author URI: http://frankiejarrett.com
  * License: GPLv2+
  * Text Domain: user-session-control
+ *
+ * Copyright: © 2015 Frankie Jarrett.
+ * License: GNU General Public License v2.0
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /**
